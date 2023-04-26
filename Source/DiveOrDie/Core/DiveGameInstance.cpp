a6685e2639d260cd69b6c8ae2feaa0b093885f63
Shin Ihan <89966726+ShinIHan@users.noreply.github.com>
2023-04-27 03:05:13 +0900

    튜토리얼 시스템 구현중

--- Source/DiveOrDie/Core/DiveGameInstance.cpp
+++ Source/DiveOrDie/Core/DiveGameInstance.cpp
@@ -2,10 +2,87 @@
 
 
 #include "DiveGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 
 void UDiveGameInstance::Init()
 {
 	Super::Init();
+	if (IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+	{
+		SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UDiveGameInstance::OnCreateSessionComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UDiveGameInstance::OnFindSessionComplete);
+			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UDiveGameInstance::OnJoinSessionComplete);
+		}
+		LOG_SCREEN("Subsystem : %s", *Subsystem->GetSubsystemName().ToString());
+	}
+}
+
+void UDiveGameInstance::CreateServer()
+{
+	LOG_SCREEN("Create Server");
+	FOnlineSessionSettings SessionSettings;
+	SessionSettings.bAllowJoinInProgress = true;
+	SessionSettings.bIsDedicated = false;
+	SessionSettings.bIsLANMatch = (IOnlineSubsystem::Get()->GetSubsystemName() != "NULL") ? false : true;
+	SessionSettings.bShouldAdvertise = true;
+	SessionSettings.bUsesPresence = true;
+	SessionSettings.NumPublicConnections = 5;
+
+	SessionInterface->CreateSession(0, FName("TestSession"), SessionSettings);
+}
+
+void UDiveGameInstance::JoinServer()
+{
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+	SessionSearch->bIsLanQuery = (IOnlineSubsystem::Get()->GetSubsystemName() == "NULL");
+	SessionSearch->MaxSearchResults = 10000;
+	SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+	SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+}
+
+void UDiveGameInstance::OnCreateSessionComplete(FName SessionName, bool bResult)
+{
+	if (bResult)
+	{
+		GetWorld()->ServerTravel("/Game/Maps/Stage_1?listen");
+	}
+}
+
+void UDiveGameInstance::OnFindSessionComplete(bool bResult)
+{
+	if (bResult)
+	{
+		TArray<FOnlineSessionSearchResult> SearchResults = SessionSearch->SearchResults;
+
+		if (SearchResults.Num())
+		{
+			SessionInterface->JoinSession(0, FName("TestSession"), SearchResults[0]);
+		}
+		else
+		{
+			LOG_SCREEN("Find Fail");
+			CreateServer();
+		}
+	}
+}
+
+void UDiveGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		FString JoinAddress = "";
+		SessionInterface->GetResolvedConnectString(SessionName, JoinAddress);
+		if (JoinAddress != "")
+		{
+			PlayerController->ClientTravel(JoinAddress, ETravelType::TRAVEL_Absolute);
+		}
+	}
+
 }
 
 void UDiveGameInstance::SetUserInfo(int difficulty, int stage, int key)
--- Source/DiveOrDie/Core/DiveGameInstance.h
+++ Source/DiveOrDie/Core/DiveGameInstance.h
@@ -2,8 +2,11 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "DiveOrDie/InGame/game.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemUtils.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "DiveGameInstance.generated.h"
 
 /**
@@ -35,7 +38,6 @@ class GAME_API UDiveGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
-	virtual void Init() override;
 	
 	void SetUserInfo(int difficulty, int stage, int key);
 
@@ -47,6 +49,30 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	int GetStage();
+
+	UPROPERTY(BlueprintReadWrite)
+		int iStageNum = 1;
+
+	UPROPERTY(BlueprintReadWrite)
+		bool bIsOnline = false;
+
+protected:
+	virtual void Init() override;
+
+	UFUNCTION(BlueprintCallable)
+		void CreateServer();
+
+	UFUNCTION(BlueprintCallable)
+		void JoinServer();
+
+	virtual void OnCreateSessionComplete(FName SessionName, bool bResult);
+	virtual	void OnFindSessionComplete(bool bResult);
+	virtual void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	TSharedPtr<FOnlineSessionSearch> SessionSearch;
+
+	IOnlineSessionPtr SessionInterface;
+
 	
 private:
 	FUserInfo _stUserInfo;
