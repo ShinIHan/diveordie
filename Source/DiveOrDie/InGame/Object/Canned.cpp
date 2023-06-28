84cb8e8309295434dc380c1eed3e1d70b10ca4fc
SeoGilJae <sgj9946@gmail.com>
2023-06-28 14:30:42 +0900

    Add Canned code

---
+++ Source/DiveOrDie/InGame/Object/Canned.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DiveOrDie/InGame/Object/Canned.h"
+#include "DiveOrDie/InGame/Character/DiveCharacter.h"
+
+// Sets default values
+ACanned::ACanned()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CannedBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = CannedBox;
+
+	CannedBox->SetBoxExtent(FVector(65.0f, 65.0f, 68.0f));
+
+	CannedMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	CannedMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+	CannedMesh->SetCollisionProfileName("Object");
+	CannedBox->SetCollisionProfileName("OverlapAllDynamic");
+
+	CannedMesh->SetRelativeScale3D(FVector(12.0f, 12.0f, 10.0f));
+	CannedMesh->SetRelativeLocation(FVector(5.0f, 22.0f, -70.0f));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CANNED_MESH(TEXT("/Game/Meshes/Canned.Canned"));
+	if (CANNED_MESH.Succeeded())
+	{
+		CannedMesh->SetStaticMesh(CANNED_MESH.Object);
+	}
+
+	CannedBox->OnComponentBeginOverlap.AddDynamic(this, &ACanned::OnOverlapBegin);
+}
+
+// Called when the game starts or when spawned
+void ACanned::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACanned::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ADiveCharacter* Character = Cast<ADiveCharacter>(OtherActor);
+	if (Character)
+	{
+		Character->UpdateTrashCount();
+		Destroy();
+	}
+}
+
+// Called every frame
+void ACanned::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/DiveOrDie/InGame/Object/Canned.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../game.h"
+#include "GameFramework/Actor.h"
+#include "Canned.generated.h"
+
+UCLASS()
+class GAME_API ACanned : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACanned();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* CannedMesh;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* CannedBox;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
